5dae18c54a2d4394418e87672d6a951c5e3b92ac
Vivek.Singh <vivek.singh@wolteskluwer.com>
2023-08-23 20:12:34 +0200

    Add project files.

---
+++ Speak.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Vivek_s_Text_To_Speech_Application
+{
+    partial class Speak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextboxSpeak = new System.Windows.Forms.RichTextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnSpeakHindi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextboxSpeak
+            // 
+            this.richTextboxSpeak.AcceptsTab = true;
+            this.richTextboxSpeak.BackColor = System.Drawing.Color.PapayaWhip;
+            this.richTextboxSpeak.Location = new System.Drawing.Point(126, 94);
+            this.richTextboxSpeak.Name = "richTextboxSpeak";
+            this.richTextboxSpeak.Size = new System.Drawing.Size(838, 263);
+            this.richTextboxSpeak.TabIndex = 0;
+            this.richTextboxSpeak.Text = "";
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSpeak.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpeak.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnSpeak.Location = new System.Drawing.Point(178, 397);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(337, 91);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Speak in English";
+            this.btnSpeak.UseVisualStyleBackColor = false;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnSpeakHindi
+            // 
+            this.btnSpeakHindi.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSpeakHindi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpeakHindi.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnSpeakHindi.Location = new System.Drawing.Point(582, 397);
+            this.btnSpeakHindi.Name = "btnSpeakHindi";
+            this.btnSpeakHindi.Size = new System.Drawing.Size(349, 91);
+            this.btnSpeakHindi.TabIndex = 2;
+            this.btnSpeakHindi.Text = "Speak in Hindi";
+            this.btnSpeakHindi.UseVisualStyleBackColor = false;
+            this.btnSpeakHindi.Click += new System.EventHandler(this.btnSpeakHindi_Click);
+            // 
+            // Speak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 594);
+            this.Controls.Add(this.btnSpeakHindi);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.richTextboxSpeak);
+            this.Name = "Speak";
+            this.Text = "Vivek\'s Text To Speech Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextboxSpeak;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnSpeakHindi;
+    }
+}
+
---
+++ Speak.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+
+namespace Vivek_s_Text_To_Speech_Application
+{
+    public partial class Speak : Form
+    {
+        private SpeechSynthesizer synthesizer; // Class level variable
+
+        public Speak()
+        {
+            InitializeComponent();
+            synthesizer = new SpeechSynthesizer();
+            this.Text = "Vivek's Text To Speech Application";
+            richTextboxSpeak.KeyDown += new KeyEventHandler(richTextboxSpeak_KeyDown); // KeyDown event subscribe karein
+
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string textToSpeak = richTextboxSpeak.Text;
+                if (!string.IsNullOrEmpty(textToSpeak))
+                {
+                    synthesizer.SpeakAsyncCancelAll(); // Pehle ki boli hui speech ko rok de
+                    synthesizer.SpeakAsync(textToSpeak); // Naya text aloud padhe
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            try
+            {
+                synthesizer.Dispose();
+                base.OnFormClosing(e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during closing: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SpeakInHindiWithEspeak(string text)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(text))
+                {
+                    System.Diagnostics.Process.Start("espeak", $"-v hi \"{text}\"");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSpeakHindi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string textToSpeak = richTextboxSpeak.Text;
+                if (!string.IsNullOrEmpty(textToSpeak))
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = "espeak",
+                        Arguments = $"-v hi \"{textToSpeak}\"",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    };
+
+                    Process process = new Process { StartInfo = startInfo };
+                    process.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // This is the overridden method to capture the F5 key press
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            try
+            {
+                if (keyData == (Keys.Control | Keys.H))
+                {
+                    btnSpeakHindi_Click(null, null);
+                    return true; // Key press ko handle kar liya
+                }
+
+                if (keyData == (Keys.Control | Keys.E))
+                {
+                    btnSpeak_Click(null, null);
+                    return true; // Key press ko handle kar liya
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData); // Default handling
+        }
+
+
+        private void richTextboxSpeak_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Control)
+                {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.C:
+                            if (richTextboxSpeak.SelectionLength > 0)
+                                richTextboxSpeak.Copy();
+                            break;
+                        case Keys.V:
+                            if (Clipboard.ContainsText())
+                            {
+                                int position = richTextboxSpeak.SelectionStart;
+                                string beforeText = richTextboxSpeak.Text.Substring(0, position);
+                                string afterText = richTextboxSpeak.Text.Substring(position);
+                                richTextboxSpeak.Text = beforeText + Clipboard.GetText() + afterText;
+                                richTextboxSpeak.SelectionStart = position + Clipboard.GetText().Length;
+                                e.Handled = true; // Yeh line add karein
+                            }
+                            break;
+                        case Keys.X:
+                            if (richTextboxSpeak.SelectionLength > 0)
+                                richTextboxSpeak.Cut();
+                            break;
+                        case Keys.A:
+                            richTextboxSpeak.SelectAll();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
